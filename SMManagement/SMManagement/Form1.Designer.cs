2662dd80ff7c444e9331db0359c5159ab669cae1
quanhn9792 <hongquansw@gmail.com>
2016-10-17 17:06:32 +0700

    Project
    Form

---
+++ SMManagement/SMManagement/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SMManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hehotn = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hethongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QLTKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QLBHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QLKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QLKHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hehotn
+            // 
+            this.hehotn.Name = "contextMenuStrip1";
+            this.hehotn.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hethongToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hethongToolStripMenuItem
+            // 
+            this.hethongToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.QLTKToolStripMenuItem,
+            this.QLBHToolStripMenuItem,
+            this.QLKToolStripMenuItem,
+            this.QLKHToolStripMenuItem});
+            this.hethongToolStripMenuItem.Name = "hethongToolStripMenuItem";
+            this.hethongToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hethongToolStripMenuItem.Text = "Hệ thống";
+            // 
+            // QLTKToolStripMenuItem
+            // 
+            this.QLTKToolStripMenuItem.Name = "QLTKToolStripMenuItem";
+            this.QLTKToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.QLTKToolStripMenuItem.Text = "Quản lý tài khoản";
+            // 
+            // QLBHToolStripMenuItem
+            // 
+            this.QLBHToolStripMenuItem.Name = "QLBHToolStripMenuItem";
+            this.QLBHToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.QLBHToolStripMenuItem.Text = "Quản lý bán hàng";
+            // 
+            // QLKToolStripMenuItem
+            // 
+            this.QLKToolStripMenuItem.Name = "QLKToolStripMenuItem";
+            this.QLKToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.QLKToolStripMenuItem.Text = "Quản lý kho";
+            // 
+            // QLKHToolStripMenuItem
+            // 
+            this.QLKHToolStripMenuItem.Name = "QLKHToolStripMenuItem";
+            this.QLKHToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.QLKHToolStripMenuItem.Text = "Quản lý khách hàng";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip hehotn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hethongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QLTKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QLBHToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QLKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QLKHToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SMManagement/SMManagement/FrmLogin.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SMManagement
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(317, 205);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "Login";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(474, 205);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tài khoản";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(371, 98);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(199, 20);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUser_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(371, 150);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(199, 20);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SMManagement.Properties.Resources.iconLogin;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(372, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Chào mừng bạn đến với hệ thống Quản lý siêu thị, \r\n\r\nmời bạn vui lòng nhập tên tr" +
+    "uy cập và mật khẩu để đăng nhập vào hệ thống!";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 239);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Name = "FrmLogin";
+            this.Text = "FrmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
